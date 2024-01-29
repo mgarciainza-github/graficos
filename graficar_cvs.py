c56e73a8e9d3736d84f4551ccc5eed7c15e178d7
MGI <mariano.garciainza@gmail.com>
2024-01-29 16:54:15 -0300

    modificaciones en yoga

--- graficar_cvs.py
+++ graficar_cvs.py
@@ -13,45 +13,47 @@ import numpy as np
 #nombre_archivo = 'site2.1_tile4_device23_-5to5_0C.csv'
 #ruta_archivo = ruta_base + ruta_especifica + nombre_archivo
 
-ruta_base = "/home/mariano/git-proyectos/marianogh03_graficos/"
+ruta_base = "/home/mariano/github/marianogh03_graficos/"
 ruta_especifica = ""
 nombre_archivo = "a.csv"
 ruta_archivo = ruta_base + ruta_especifica + nombre_archivo
 
 df = pd.read_csv( ruta_archivo )
-print(df)
+#print(df)
 
-df.rename(columns = {'time':'date'}, inplace = True)
-print(df.columns)
-
+df.rename(columns = {'time':'Date'}, inplace = True)
+df.rename(columns = {'Y[0]':'Y00', 'Y[1]':'Y01', 'Y[2]':'Y02','Y[3]':'Y03','Y[4]':'Y04' }, inplace = True)
 
 # agregar columna de tiempo en segundos time(s) siendo que la primera columna es un string con la fecha y hora: "31/12/1903  21:00:00.000000"
 date_str = df.iat[0,0]
-t_cero = 10*3600*int(date_str[12]) + 3600*int(date_str[13]) + 10*60*int(date_str[15]) + 60*int(date_str[16]) + 10*int(date_str[18]) + int(date_str[19]) + 0.1*int(date_str[21])
-print(t_cero)
+t_zero = 10*3600*int(date_str[12]) + 3600*int(date_str[13]) + 10*60*int(date_str[15]) + 60*int(date_str[16]) + 10*int(date_str[18]) + int(date_str[19]) + 0.1*int(date_str[21])
 date_str=df.iat[11,0]
-print(date_str)
-print( 10*3600*int(date_str[12]) + 3600*int(date_str[13]) + 10*60*int(date_str[15]) + 60*int(date_str[16]) + 10*int(date_str[18]) + int(date_str[19]) + 0.1*int(date_str[21]) )
-print("---")
-print(df.loc[:,["date"]])
-print("---")
+
 largo = df.shape[0]
+t_list=[]
 for x in range(largo):
     date_str=df.iat[x,0]
-    print(date_str)
-    time = 10*3600*int(date_str[12]) + 3600*int(date_str[13]) + 10*60*int(date_str[15]) + 60*int(date_str[16]) + 10*int(date_str[18]) + int(date_str[19]) + 0.1*int(date_str[21]) 
-    print( time - t_cero )
-    print("---")
+    #print(date_str)
+    t_value = 10*3600*int(date_str[12]) + 3600*int(date_str[13]) + 10*60*int(date_str[15]) + 60*int(date_str[16]) + 10*int(date_str[18]) + int(date_str[19]) + 0.1*int(date_str[21]) 
+    t_list.append(str(t_value - t_zero))
+
+#print('---')
+#print(t_list)
+
+#print('---')
+df.insert(1, "Time(s)", t_list, True)
+print(df)
 
 
-
-#agregar una columna y2 que sea el abs(i_a) para luego graficar en semilogy
+#graficos:
 
 #df.plot(x='v_a', y='i_a', logy=True, title=nombre_archivo, marker='o', linestyle='dashed', color='red')
 #df.plot(x='v_a', y='i_a', title=nombre_archivo, marker='x', linestyle='dashed', color='blue')
 
 
-#df.plot(x='time', y='Y[0]', logy=True, title=nombre_archivo, marker='o', linestyle='dashed', color='red')
+df.plot(x='Time(s)', y='Y00', logy=True, title=nombre_archivo, marker='o', linestyle='dashed', color='red')
+df.plot(x='Time(s)', y='Y[8]', logy=True, title=nombre_archivo, marker='o', linestyle='dashed', color='blue')
+df.plot(x='Time(s)', y='Y[9]', logy=True, title=nombre_archivo, marker='o', linestyle='dashed', color='green')
 
-#plt.show(block=False)
-#plt.show()
+plt.show(block=False)
+plt.show()
